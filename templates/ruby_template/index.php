576d47c64a71c88bd9a34ab37397bd12a66d385c
Tran Cong <trancong.joomla@gmail.com>
2018-08-28 17:33:53 +0700

    add method section

--- templates/ruby_template/index.php
+++ templates/ruby_template/index.php
@@ -525,5 +525,51 @@ $user = JFactory::getUser();
 </section>
 <!--haha-->
 
+<section class="method">
+    <div class="method__top">
+        <div class="container"><h2 class="method__title">Proven Methods That Produce Real Results</h2>
+            <div class="row">
+                <div class="col-lg-6 col-md-12">
+                    <div class="method__grey-title"> Industry Expertise + Laser-Focused Team =</div>
+                    <div class="method__green-title">Success</div>
+                </div>
+                <div class="col-lg-6 col-md-12">
+                    <div class="method__text"> Businesses know that a single, full-time marketing manager simply cannot
+                        perform all the roles in digital marketing. Comrade Web Agency positions itself as your <span>digital marketing partner</span>:
+                        we cover all aspects of your website, branding, digital marketing strategy, and data analysis,
+                        all while owning our results (<span>leads, calls, sales, conversions, targeted traffic</span>,
+                        etc).
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="method__wrap">
+        <div class="method-item ">
+            <div class="method-item__text"><h3><i class="icon-chart-growth"></i> <span>Industry Expertise and Strategy Development</span>
+                </h3>
+                <p><span style="font-weight: 400;">With almost 10 years in web design, development, and digital marketing, we’ve worked on over 200 projects across dozens of industries, honing our skills and growing our craft since. As your partner, we will assign a brand strategist with experience in your industry to understand your business, develop an effective long-term digital marketing strategy, oversee execution, and provide ongoing data-driven adjustments along the way.</span>
+                </p></div>
+            <div class="method-item__img"><img
+                        src="<?php echo JUri::root() . 'images/custom' ?>/webdesign-stage1-1024x768.jpg"
+                        alt=""></div>
+        </div>
+        <div class="method-item method-item--reverse">
+            <div class="method-item__text"><h3><i class="icon-pencil-ruler2"></i>
+                    <span>Result-Driven Production Team</span></h3>
+                <p>With CWA, you’re equipped with a team of driven professionals: project managers, marketing
+                    strategists, web designers, web developers, QA team, PPC and SEO specialists, copywriters, brand
+                    strategists, and social media experts. Each component of this digital engine works side by side to
+                    ensure that your project is delivered <strong>on time</strong> and in a <strong>cost-effective
+                        manner</strong>—without ever sacrificing quality.</p></div>
+            <div class="method-item__img"><img
+                        src="<?php echo JUri::root() . 'images/custom' ?>/results-driven-1024x683.jpg"
+                        alt=""></div>
+        </div>
+    </div>
+</section>
+
+
+
 </body>
 </html>
